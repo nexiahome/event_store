9a069afb0d8534bc155c5fce132cd606513ea3e4
Joey Geiger <joey.geiger@tranetechnologies.com>
2021-01-07 09:36:05 -0700

    fix: add mock_redis and ruby2_keywords to spec_helper
    This ensures we're loading the correct libraries to get the specs
    passing when they use mock_redis.
    
    [IOTPLAT-710]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,8 @@ require "ostruct"
 require "rspec"
 
 require "event_store"
+require "ruby2_keywords"
+require "mock_redis"
 
 # connect to the test db for the gem, create to ensure exists, delete and re-create
 EventStore.postgres("test", "test_events", "event_store_gem_test")
